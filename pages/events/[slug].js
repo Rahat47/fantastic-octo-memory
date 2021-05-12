9758ccef325719c328dd85b23316da106cbf9018
Rahat <djrayhan8@gmail.com>
2021-05-12 20:05:06 +0600

    let me see

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,6 +1,9 @@
 import Layout from "@/components/Layout";
+import { useRouter } from "next/router";
 
 export default function EventPage() {
+  const router = useRouter()
+  console.log(router.pathname.slug);
   return (
     <Layout title="Single Event | Slug">
       <h1>Single Event Page</h1>
